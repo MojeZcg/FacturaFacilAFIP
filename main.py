3bf6ff003d063dcb2eeb8d0420cc344305c7dbaf
Moje <jsMonte31@gmail.com>
2024-06-29 19:32:52 -0300

    add: dynamic path

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import threading
 import time
 from datetime import datetime
+from pathlib import Path
 from tkinter.messagebox import showinfo
 
 import ttkbootstrap as ttk
@@ -27,7 +28,10 @@ session = Session()
 fecha_actual = datetime.now()
 nombre_carpeta = fecha_actual.strftime('%d de %B %Y')
 
-download_path = os.path.join(r'C:\Users\w10\Desktop\Facturas', nombre_carpeta)
+default_path = Path(os.getenv('DOWNLOAD_PATH'))
+
+download_path = os.path.join(default_path, nombre_carpeta)
+
 
 if not os.path.exists(download_path):
     os.makedirs(download_path)
@@ -53,7 +57,7 @@ def start_chrome():
         "safebrowsing.enabled": True
     }
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(service=service, options=options)
@@ -243,6 +247,7 @@ def in_thread(client_option, client_id, option, products):
     )
     thread.start()
 
+
 class ProgressWindow:
     """
     Clase que representa una ventana de progreso con una barra de progreso.
@@ -308,6 +313,7 @@ class ProgressWindow:
         if self.progress_window:
             self.progress_window.destroy()
 
+
 def put_all_items(driver, products):
     """
     Ingresa una lista de productos en un formulario web usando Selenium.
@@ -355,6 +361,7 @@ def put_all_items(driver, products):
                     )
                 )
             ).click()
+
 
 def realizar_operacion(driver, client_option, client_id, option, products):
     """
@@ -448,6 +455,7 @@ def realizar_operacion(driver, client_option, client_id, option, products):
         siguiente(driver)
 
         progress.set_progress(80)
+
         try:
             WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable(
@@ -460,20 +468,23 @@ def realizar_operacion(driver, client_option, client_id, option, products):
 
             progress.set_progress(85)
 
+            # Acceptar la factura
             WebDriverWait(driver, 10).until(EC.alert_is_present())
-            driver.switch_to.alert.accept()
+            # driver.switch_to.alert.accept()
 
-            progress.set_progress(90)
+            # progress.set_progress(90)
 
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     '//input[@type="button" and @value="Imprimir..."]')
-                )
-            ).click()
+            # WebDriverWait(driver, 10).until(
+            #     EC.element_to_be_clickable(
+            #         (By.XPATH,
+            #          '//input[@type="button" and @value="Imprimir..."]')
+            #     )
+            # ).click()
 
             progress.set_progress(95)
+
             time.sleep(6)
+
             progress.set_progress(100)
 
         except RuntimeError as e:
